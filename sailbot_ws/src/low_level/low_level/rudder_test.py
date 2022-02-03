f5f3d47721a7b9a6dc71382d99dd6a7dabebbfcc
AndrewDel02 <andrewdel82@gmail.com>
2022-02-02 23:50:16 -0500

    Ready for first test

--- sailbot_ws/src/low_level/low_level/rudder_test.py
+++ sailbot_ws/src/low_level/low_level/rudder_test.py
@@ -1,23 +1,33 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+import json
 
 
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('Rudder_Test')
-        self.publisher_ = self.create_publisher(String, 'testing', 10)
-        timer_period = 0.5  # seconds
+        self.publisher_ = self.create_publisher(String, 'pwm_control', 10)
+        timer_period = 15  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
+    def make_json_string(self, json_msg):
+        json_str = json.dumps(json_msg)
+        message = String()
+        message.data = json_str
+        return message
+
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+        # msg = String()
+        angles = [20, 50, 70]
+        # msg.data = 'Hello World: %d' % self.i
+        rudder_json = {"channel": "8", "angle": angles[self.i]}
+        rudder_string = self.make_json_string(rudder_json)
+        self.publisher_.publish(rudder_string)
+        self.get_logger().info('Publishing: "%s"' % rudder_string)
+        self.i = (self.i+1) % 3
 
 
 def main(args=None):
