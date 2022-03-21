8fcbe120cc25409f7b1a15d6d3df1bb318ab2f1a
AndrewDel02 <andrewdel82@gmail.com>
2022-03-21 19:58:40 -0400

    I am very smart and pasted the d_heading code to the wrong file. this is now fixed

--- sailbot_ws/src/low_level/low_level/desired_heading_publisher.py
+++ sailbot_ws/src/low_level/low_level/desired_heading_publisher.py
@@ -10,14 +10,14 @@ class DesiredHeading(Node):
     def __init__(self):
         super().__init__('Desired_Heading')
         self.publisher_ = self.create_publisher(Int64, 'd_heading', 10)
-        self.subscription = self.create_subscription(String, '/airmar')
-        timer_period = 10  # seconds
+        self.subscription = self.create_subscription(String, 'airmar_data', self.listener_callback, 15)
+        timer_period = 1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
         self.current_heading = 0
 
     def timer_callback(self):
-        msg = String()
+        msg = Int64()
         print("Current Heading: ", self.current_heading)
         d_heading = input("Desired Heading: ")
         msg.data = int(d_heading)
@@ -25,7 +25,7 @@ class DesiredHeading(Node):
         self.get_logger().info('Publishing: "%s"' % msg)
 
     def listener_callback(self, msg):
-        msg_dict = json.loads(msg)
+        msg_dict = json.loads(msg.data)
         if "magnetic-sensor-heading" in msg_dict:
             self.current_heading = float(msg_dict["magnetic-sensor-heading"])
 
