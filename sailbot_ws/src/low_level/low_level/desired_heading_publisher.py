26771744d043c7ec59cfab87ab317540b1bdded5
AndrewDel02 <andrewdel82@gmail.com>
2022-03-21 19:09:44 -0400

    added current heading publishing to d_heading node

--- sailbot_ws/src/low_level/low_level/desired_heading_publisher.py
+++ sailbot_ws/src/low_level/low_level/desired_heading_publisher.py
@@ -1,22 +1,33 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from std_msgs.msg import Int64
+import json
 
 
 class DesiredHeading(Node):
 
     def __init__(self):
         super().__init__('Desired_Heading')
-        self.publisher_ = self.create_publisher(String, 'd_heading', 10)
-        timer_period = 1000  # seconds
+        self.publisher_ = self.create_publisher(Int64, 'd_heading', 10)
+        self.subscription = self.create_subscription(String, '')
+        timer_period = 10  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
+
+        self.current_heading = 0
 
     def timer_callback(self):
         msg = String()
+        print("Current Heading: ", self.current_heading)
         d_heading = input("Desired Heading: ")
-        msg.data = d_heading
+        msg.data = int(d_heading)
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg)
+
+    def listener_callback(self, msg):
+        msg_dict = json.loads(msg)
+        if "magnetic-sensor-heading" in msg_dict:
+            self.current_heading = float(msg_dict["magnetic-sensor-heading"])
 
 
 def main(args=None):
