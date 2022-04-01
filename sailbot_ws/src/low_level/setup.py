fab15a2eae195da6ed8a6b4f13519c11c4842b30
AndrewDel02 <andrewdel82@gmail.com>
2022-04-01 18:17:37 -0400

    added to setup file

--- sailbot_ws/src/low_level/setup.py
+++ sailbot_ws/src/low_level/setup.py
@@ -24,7 +24,8 @@ setup(
         'console_scripts': [
             'rudder_test = low_level.rudder_test:main',
             'desired_heading = low_level.desired_heading_publisher:main',
-            'rudder_control = low_level.rudder_control:main',
+            'rudder_control_discontinuous = low_level.rudder_control_discontinuous:main',
+            'rudder_control_continuous = low_level.rudder_control_continuous:main',
         ],
     },
 )
